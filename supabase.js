9ea640fb0a8165d21433fc46c600a4502f763bd0
GauraviKamat <gauravikamat777@gmail.com>
2026-07-29 00:26:52 +0530

    Connecting supabase db

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// Paste your Supabase project credentials below.
+// Find these in your Supabase project under: Settings -> API
+
+const SUPABASE_URL = 'https://suofltijgjkemeqteoui.supabase.co'; // e.g. https://xxxxx.supabase.co
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1b2ZsdGlqZ2prZW1lcXRlb3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNTA4ODcsImV4cCI6MjEwMDgyNjg4N30.CceFxuPZhfuwFtBtt9iiQUqdpfLiaHSQEtjNOM4MPME';
